454ba2d0730a2d88b17cb74f25018736fedb6306
tuanml1111 <tuanminhst319@gmail.com>
2025-04-11 18:30:30 +0700

    Delete firmware directory

--- firmware/boot.py
+++
@@ -1,9 +0,0 @@
-# This file is executed on every boot (including wake-boot from deepsleep)
-import gc
-import webrepl
-
-# Enable garbage collection
-gc.collect()
-
-# Uncomment the line below to start WebREPL
-# webrepl.start()
